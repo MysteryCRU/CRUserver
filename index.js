00217c966ea0c5eab428b58ee4edf2e59b52630d
athattayathu <anil@Anils-MBP.att.net>
2015-11-25 17:35:02 -0800

    still modifying the tokens to store the value for pushing

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var app = require('express')();
-var campuses = require('./campuses/campuses.js');
+//var campuses = require('./campuses/campuses.js');
 var events = require('./events/events.js');
+var push = require('./services/pushServices.js');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+//app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 // I don't know what should happen when root is requested... so I'm leaving this
 app.get('/', function (req, res, next) {
@@ -28,5 +33,25 @@ app.get('/events/:id', function (req, res) {
 	events.getEventById(req, res);
 });
 
+app.post('/push/register', function(req, res){
+      try{
+         console.log(req.body);
+         push.register('lol',req.body);
+      } catch (e){
+         console.log("error registering "+e);
+         res.sendStatus(e);
+         return;
+      }
+      console.log("logged register");
+      res.sendStatus(200);                    
+});
+
+app.post('/push', function(req, res){
+      push.sendCompiledPush();
+});
+
+app.post('/push/unregister', function(req, res){
+      push.unregister();
+});
 console.log('Yo this test worked sorta');
 app.listen(8080);
