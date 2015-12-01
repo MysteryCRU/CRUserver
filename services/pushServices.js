b8f802ee57d5c37eefae4f6b5c8220dd62597863
athattayathu <anil@pcp114830pcs.wireless.calpoly.edu>
2015-11-30 20:39:18 -0800

    push notification serverside

--- services/pushServices.js
+++ services/pushServices.js
@@ -6,20 +6,72 @@ var SENDER = new gcm.Sender('AIzaSyDNwhiWASCgUcrVhHhE0IvWfQFZZic3gyg');
 //faking the entry into DB
 var push;
 /**
+* register push token to specific user
 * PushToken object should come in the form 
 * {
 *   token:
 *   type:'android'|'ios'|'win'
 * }
 */
-function register(user, pushToken){
+function registerPushDevice(userId, pushToken, db){
+   if(!pushToken.token || !pushToken.type){
+      throw 'invalid token object' + JSON.stringify(pushToken);
+   }
+   /*var doc = db.get('user').findOne({'_id':userId});
+   var cache = [];
+   var val = JSON.stringify(doc, function(key, value) {
+       if (typeof value === 'object' && value !== null) {
+           if (cache.indexOf(value) !== -1) {
+               // Circular reference found, discard key
+               return;
+           }
+           // Store value in our collection
+           cache.push(value);
+       }
+       return value;
+   });
+   cache = null; // Enable garbage collection
+   console.log("found"+val);*/
+
+   db.get('users').update({_id: userId}, {$set: { 'notifications.pushToken': pushToken} } , function(err, count, status){
+      console.log("registering for"+userId+"...");
+      if(err){
+         console.log(err);
+      }
+      console.log("updated "+count+" results with a status "+status+" and err "+err);
+   });
+
+      
+   //});
+      /*console.log(result);
+      if(result.nModified == 0){
+         console.log('could not add pushtoken '+JSON.stringify(pushToken)+' to userId '+JSON.stringify(userId)+"count :"+count);
+         return new Error('could not add pushtoken '+JSON.stringify(pushToken)+' to userId '+JSON.stringify(userId)+"count :"+count);
+      }*/
    //TODO insert into token DB
    console.log(pushToken);
    push = pushToken;
+   //TODO remove this
+   /*var message = new gcm.Message();
+   message.addNotification('title','first push');
+   message.addNotification('body', 'lolol');
+   sendCompiledPush(message, [push.token]);
+
+   */
 }
 
-function unregister(user){
-   push = '';
+function unregisterPushDevice(userId, db){
+
+   db.get('users').updateById( userId, {$unset: { pushToken}}, function(err, count){
+   
+      if(err){
+         throw err;
+      } else if(count <= 0 ) {
+         throw 'could not remove pushtoken'+'from userId'+JSON.stringify(userId);
+      }
+      
+   });
+
 }
 
 /**
@@ -35,6 +87,7 @@ function sendNotification(message,criteria){
 *Send a gcm.message to all tokens specified in array
 */
 function sendCompiledPush(message, regTokens){
+   
    if(regTokens.length > 1000){
       throw "too many tokens" + regTokens.length;
    }
@@ -46,8 +99,8 @@ function sendCompiledPush(message, regTokens){
 }
 
 exports.sendNotification = sendNotification;
-exports.register = register;
-exports.unregister = unregister;
+exports.register = registerPushDevice;
+exports.unregister = unregisterPushDevice;
 //TODO remove this before going live
 exports.sendCompiledPush = sendCompiledPush;
 
