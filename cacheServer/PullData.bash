9d18e266d0d36d5437c7d40993b7e5cca81f7471
Ian Dunn <iondune@gmail.com>
2016-01-29 18:24:43 -0500

    Delete JSON temp directory after loading

--- cacheServer/PullData.bash
+++ cacheServer/PullData.bash
@@ -8,7 +8,6 @@ collections='campus events posts users ministries summermissions ministryteams'
 # First, download the JSON for each collection from the official cru database
 echo " --- Downloading JSON ---"
 mkdir -p JSON
-rm -rf JSON/*
 for collection in $collections
 do
     mongo ds049754.mongolab.com:49754/heroku_s75tvv20 -u capstone-class -p capstone-2015-2016 --quiet --eval "function printResult(r) { print(tojson(r)); } db.$collection.find().forEach(printResult);" > "JSON/$collection.json"
@@ -20,6 +19,7 @@ for collection in $collections
 do
     mongoimport --db cru --collection $collection --file "JSON/$collection.json" --drop
 done
+rm -rf JSON/*
 
 # Finally, run the mongo script to add additional views to the local database
 echo " --- Updating fields ---"
