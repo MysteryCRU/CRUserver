69253463b6e6a0012556a7380368b96d232271ee
Ian Dunn <iondune@gmail.com>
2016-01-29 18:34:39 -0500

    Use parameters for mongo server, password, and db

--- cacheServer/PullData.bash
+++ cacheServer/PullData.bash
@@ -4,13 +4,20 @@
 # Config - Which collections to copy over
 collections='campus events posts users ministries summermissions ministryteams'
 
+if [ -z $3 ]
+then
+	echo "usage: ./PullData.bash <server> <password> <database>"
+	exit 1
+fi
+
+echo "Connection to server '$1', database '$3' ..."
 
 # First, download the JSON for each collection from the official cru database
 echo " --- Downloading JSON ---"
 mkdir -p JSON
 for collection in $collections
 do
-    mongo ds049754.mongolab.com:49754/heroku_s75tvv20 -u capstone-class -p capstone-2015-2016 --quiet --eval "function printResult(r) { print(tojson(r)); } db.$collection.find().forEach(printResult);" > "JSON/$collection.json"
+    mongo "$1" -u $3 -p "$2" --quiet --eval "function printResult(r) { print(tojson(r)); } db.$collection.find().forEach(printResult);" > "JSON/$collection.json"
 done
 
 # Now upload that JSON to the local database
